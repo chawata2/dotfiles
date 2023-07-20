bd83c3be2dfb01ca6d7329e9636cdc880e805a87
chappiem <cha39881@gmail.com>
2023-07-20 23:34:33 +0900

    feat: local only config

---
+++ config/wezterm/local.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local module = {}
+
+function module.apply_local_config(config)
+	-- local only config
+
+	-- config.font = wezterm.font("PlemolJP Console NF")
+end
+
+return module
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -15,92 +15,21 @@ config.font = wezterm.font("PlemolJP Console NF")
 config.scrollback_lines = 10000
 
 -- For example, changing the color scheme:
--- config.color_scheme = "Sakura"
--- config.color_scheme = "Spacemacs (base16)"
--- config.color_scheme = "GitHub Dark"
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "tokyonight"
 
 -- 背景透過
-config.window_background_opacity = 0.85
-
--- 最初からフルスクリーンで起動
-local mux = wezterm.mux
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():toggle_fullscreen()
-end)
+-- config.window_background_opacity = 0.85
 
 config.inactive_pane_hsb = {
 	saturation = 0.9,
 	brightness = 0.7,
 }
 
--- tsl
--- Todo
+-- keymap
+config.keys = require("keymap")
 
--- ショートカットキー設定
-config.keys = {
-	-- Alt(Opt)+Shift+Fでフルスクリーン切り替え
-	{
-		key = "f",
-		mods = "SHIFT|META",
-		action = wezterm.action.ToggleFullScreen,
-	},
-	-- Ctrl+Shift+tで新しいタブを作成
-	{
-		key = "t",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	-- Ctrl+Shift+%で新しいペインを作成(画面を水平分割)
-	{
-		key = "%",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	-- Ctrl+Shift+"で新しいペインを作成(画面を垂直分割)
-	{
-		key = '"',
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	-- ペインを閉じる
-	{
-		key = "e",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "p",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		key = "n",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "l",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "k",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-}
+-- local config
+require("local").apply_local_config(config)
 
 -- and finally, return the configuration to wezterm
 return config
