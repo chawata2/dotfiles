b8470ccd949bfec25c2f9dc858bda4c00d709c01
chawata <cha39881@gmail.com>
2023-07-29 06:14:26 +0900

    rm: local.zsh

--- zsh.d/local.zsh
+++
@@ -1,8 +0,0 @@
-# asdf
-. "$HOME/.asdf/asdf.sh"
-fpath=($HOME/.asdf/completions $fpath)
-autoload -Uz compinit && compinit
-
-# welcome
-# vmstat | cowsay -n -f dragon | lolcat --force
-figlet "on Lubuntu!" | cowsay -n -f dragon | lolcat --force
