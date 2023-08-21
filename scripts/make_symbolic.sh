ee6b64bb1c1a2e339e9871e2b9a631cc2e25a948
chawata-wsl <cha39881@gmail.com>
2023-08-21 23:44:14 +0900

    script

--- scripts/make_symbolic.sh
+++ scripts/make_symbolic.sh
@@ -9,6 +9,3 @@ fi
 ln -vfsn $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -vfsn $HOME/dotfiles/zshrc $HOME/.zshrc
 ln -vfsn $HOME/dotfiles/sheldon $HOME/.config/
-ln -vfsn $HOME/dotfiles/nvim $HOME/.config/
-ln -vfsn $HOME/dotfiles/evremap $HOME/.config/
-ln -vfsn $HOME/dotfiles/odc.json $HOME/.config/
