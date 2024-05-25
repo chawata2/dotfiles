27d30c8c184541de194c444ed5f460dcc43c9c94
chawata <cha39881@gmail.com>
2024-05-25 18:31:36 +0900

    remove files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,28 +1,90 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
-local config = {}
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
-config.scrollback_lines = 10000
--- config.color_scheme = "tokyonight"
+-- This is where you actually apply your config choices
+-- config.color_scheme = 'Atom'
 
 -- 背景透過
--- config.window_background_opacity = 0.85
+config.window_background_opacity = 0.75
 
-config.inactive_pane_hsb = {
-	saturation = 0.9,
-	brightness = 0.7,
+config.font_size = 9.5
+config.audible_bell = "Disabled"
+config.scrollback_lines = 3499
+
+config.default_prog = { "wsl.exe", "--distribution", "ubuntu", "--cd", "~" }
+-- タブ横の+を右クリックすると表示されるメニュー
+config.launch_menu = {
+	{
+		args = { "wsl.exe", "--distribution", "ubuntu", "--cd", "~" },
+		label = "wsl"
+	},
+	{
+		label = "cmd",
+	},
 }
 
--- keymap
-config.keys = require("keymap")
+local act = wezterm.action
+config.keys = {
+	{
+		key = "t",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "y",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SpawnTab("DefaultDomain"),
+	},
+	{
+		key = "%",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = '"',
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "e",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "p",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
 
 -- local config
 local function file_exists(name)
