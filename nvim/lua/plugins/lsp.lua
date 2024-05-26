c0d13c6af162c8efcfddf7f278158755c8a41dbd
chawata <cha39881@gmail.com>
2024-05-26 23:39:12 +0900

    nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -24,15 +24,5 @@ return {
 				null_ls.builtins.formatting.stylua
 			})
 		end
-	},
-	{
-		'nvimdev/lspsaga.nvim',
-		config = function()
-			require('lspsaga').setup({})
-		end,
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter',
-			'nvim-tree/nvim-web-devicons',
-		}
 	}
 }
