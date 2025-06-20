0bc8e9ce167970d28e9ea7c53f55d4c12cf4b2a0
chawaRA <cha39881@gmail.com>
2025-06-20 23:41:21 +0900

    remove neovim

--- nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("n", "x", '"_x', { noremap = true })
-vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true })
